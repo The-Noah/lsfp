90604cbd27d2d8b821eeff2f7fb4975741016e80
MattPlays <markgpmcpe@gmail.com>
2021-01-06 12:05:12 -0500

    Fix Issue #21
    To use this Feature Simply
    "".to_owned().reset() for example
    or
    String::from("").reset()
    Color Functions are
      fn reset(&self) -> Self;
      fn bright(&self, flags: &Flags) -> Self;
      fn underline(&self) -> Self;
    fn green(&self, flags: &Flags) -> Self;  fn orange(&self, flags: &Flags) -> Self fn cyan(&self, flags: &Flags) -> Self;
    fn white(&self, flags: &Flags) -> Self;
    fn grey(&self, flags: &Flags) -> Self;
    fn custom(&self, color: &str, flags: &Flags) -> Self;

--- src/color.rs
+++ src/color.rs
@@ -1,5 +1,4 @@
 use crate::utils::Flags;
-
 pub const RESET: &str = "\x1b[0m";
 pub const BRIGHT: &str = "\x1b[1m";
 pub const UNDERLINE: &str = "\x1b[4m";
@@ -8,6 +7,74 @@ pub const ORANGE: &str = "\x1b[33m";
 pub const CYAN: &str = "\x1b[36m";
 pub const WHITE: &str = "\x1b[37m";
 pub const GREY: &str = "\x1b[90m";
+pub trait ColorExt {
+  fn reset(&self) -> Self;
+  fn bright(&self, flags: &Flags) -> Self;
+  fn underline(&self) -> Self;
+  fn green(&self, flags: &Flags) -> Self;
+  fn orange(&self, flags: &Flags) -> Self;
+  fn cyan(&self, flags: &Flags) -> Self;
+  fn white(&self, flags: &Flags) -> Self;
+  fn grey(&self, flags: &Flags) -> Self;
+  fn custom(&self, color: &str, flags: &Flags) -> Self;
+}
+impl ColorExt for String {
+  fn reset(&self) -> Self {
+    format!("{}{}", String::from(RESET), self)
+  }
+  fn bright(&self, flags: &Flags) -> Self {
+    if flags.no_color {
+      self.to_string()
+    } else {
+      format!("{}{}", String::from(BRIGHT), self)
+    }
+  }
+  fn underline(&self) -> Self {
+    format!("{}{}", String::from(UNDERLINE), self)
+  }
+  fn green(&self, flags: &Flags) -> Self {
+    if flags.no_color {
+      self.to_string()
+    } else {
+      format!("{}{}", String::from(GREEN), self)
+    }
+  }
+  fn orange(&self, flags: &Flags) -> Self {
+    if flags.no_color {
+      self.to_string()
+    } else {
+      format!("{}{}", String::from(ORANGE), self)
+    }
+  }
+  fn cyan(&self, flags: &Flags) -> Self {
+    if flags.no_color {
+      self.to_string()
+    } else {
+      format!("{}{}", String::from(CYAN), self)
+    }
+  }
+  fn white(&self, flags: &Flags) -> Self {
+    if flags.no_color {
+      self.to_string()
+    } else {
+      format!("{}{}", String::from(WHITE), self)
+    }
+  }
+  fn grey(&self, flags: &Flags) -> Self {
+    if flags.no_color {
+      self.to_string()
+    } else {
+      format!("{}{}", String::from(GREY), self)
+    }
+  }
+  fn custom(&self, color: &str, flags: &Flags) -> Self {
+    if flags.no_color {
+      self.to_string()
+    } else {
+      format!("{}{}", String::from(color), self)
+    }
+  }
+}
 
 pub fn get_color(color: &str, flags: &Flags) -> String {
   if flags.no_color {
