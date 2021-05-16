387f0e004b03d813d508fd09afd3528ec968b0fb
Hipy Cas <hipycas@gmail.com>
2021-05-17 01:38:57 +0200

    Fix error when running tree on directories

--- src/main.rs
+++ src/main.rs
@@ -144,7 +144,8 @@ fn main() {
   }
 
   let raw_path = args.pop().unwrap_or_else(|| String::from("."));
-  let path_to_scan = path::Path::new(raw_path.as_str());
+  let path_to_scan = path::Path::new(raw_path.as_str()).canonicalize().die("Unable to canonicalize path", &flags);
+  let path_to_scan = path_to_scan.as_path();
 
   if !path_to_scan.exists() {
     println!("File or directory does not exist");
